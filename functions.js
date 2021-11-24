36440d756d36cf1eb0e65b92be6c931ee3cc6870
Kaide Joseph-Wright <32374224+ked9268@users.noreply.github.com>
2021-11-24 20:00:58 +0000

    update

--- functions.js
+++ functions.js
@@ -4,10 +4,9 @@
         console.log(print);
         document.getElementById("number2").value = " ";
     }
-   
-    function print() {
-        console.log("Hello World!");
-    }
+	function bounce(){
+		alert('See me again!');
+	}
     function changeTextOperation() {
         document.getElementById("randomGeneratorText").remove();
         var input = document.createElement("input");
@@ -34,85 +33,27 @@
         return randomNumber;
     }
     function showDate() {
-        console.log(Date());
-        document.getElementById("showDate").innerHTML = Date();
+		setInterval(function(){ 
+		document.getElementById("showDate").innerHTML = Date()
+		},1000);
     }
     function checkSize() {
         var c = window.innerWidth;
         var d = window.innerHeight;
         var e = navigator.platform;
         //document.getElementById("body").setAttribute("max-width", c + "px");
-         document.getElementById("width").innerHTML = "width: " + c + "," + "height: " + d + "  " + e;
+        document.getElementById("width").innerHTML = "width: " + c + "," + "height: " + d + "  " + e;
+		showDate();
     }
-    function loadGallery() {
-        //document.getElementById("body").innerHTML = '';
-    }
-   function loadContactForm() {
-        // Remove Tags!
-        // document.getElementById("image_1").remove();
-        goHome();
-
-        //Add tags!
-        var insert = document.createElement('form');
-        insert.style.position = "relative";
-        insert.style.top = "400px";
-        // var insertnode = document.createTextNode("E");
-        // insert.appendChild(insertnode);
-        var new_line = document.createElement('br');
-        insert.appendChild(new_line);
-        var label = document.createElement('label');
-        label.innerHTML = "First Name";
-        insert.appendChild(label);
-        var text_area = document.createElement('textarea');
-        insert.appendChild(text_area);
-        insert.appendChild(new_line);
-        var label_lastName = document.createElement('label');
-        label_lastName.innerHTML = "Last Name";
-        insert.appendChild(label_lastName);
-        var text_area_last_name = document.createElement('textarea');
-        insert.appendChild(text_area_last_name);
-        document.getElementById("body").appendChild(insert);
-    }
-    function loadMenuBar() {
-        document.getElementById("body");
-        var home = document.createElement('input');
-        home.setAttribute("type", "button");
-        home.setAttribute("value", "Home");
-        home.setAttribute("onclick", "landingpage.html");
-        document.getElementById("body").appendChild(home);
-        var removeImages = document.createElement('input');
-        removeImages.setAttribute("type", "button");
-        removeImages.setAttribute("value", "RemoveImages");
-        home.setAttribute("onclick", "removeImages()");
-        document.getElementById("body").appendChild(removeImages);
-        var contact = document.createElement('input');
-        contact.setAttribute("type", "button");
-        contact.setAttribute("value", "Contact");
-        contact.setAttribute("onclick", "loadContactForm()");
-        document.getElementById("body").appendChild(contact);
-        var gallery = document.createElement('input');
-        gallery.setAttribute("type", "button");
-        gallery.setAttribute("value", "Gallery");
-        gallery.setAttribute("onclick", "loadGallery()");
-        document.getElementById("body").appendChild(gallery);
-        var games = document.createElement('input');
-        games.setAttribute("type", "button");
-        games.setAttribute("value", "Games");
-        games.setAttribute("onclick", "  ");
-        document.getElementById("body").appendChild(games);
-        var pictures = document.createElement('input');
-        pictures.setAttribute("type", "button");
-        pictures.setAttribute("value", "Pictures");
-        pictures.setAttribute("onclick", "   ");
-        document.getElementById("body").appendChild(pictures);
-        var videos = document.createElement('input');
-        videos.setAttribute("type", "button");
-        videos.setAttribute("value", "videos");
-        videos.setAttribute("onclick", "   ");
-        document.getElementById("body").appendChild(videos);
-
-    }
-    function goHome() {
-        document.getElementById("body").innerHTML = '';
-        loadMenuBar();
-     }
+	function  relocate(){
+		var edit = document.getElementById("circle");
+		var value = window.getComputedStyle( edit ,null).getPropertyValue('background-color'); 
+		console.log("relocate method initiated");
+		console.log(value);
+		if(value === "rgb(255, 0, 0)"){
+			edit.style.backgroundColor = "rgb(0, 255, 0)";
+		}
+		else{
+			edit.style.backgroundColor = "rgb(255, 0, 0)";
+		}	
+	}
